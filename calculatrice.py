4d75326aa193ee366f9e5c3fff472512345e69e2
adrienlanoe5 <adrienlanoe5@gmail.com>
2024-10-24 11:15:00 +0200

    test

---
+++ calculatrice.py
@@ -0,0 +1,15 @@
+import tkiteasy
+
+def addition(a,b):
+    return a+b
+
+def division(a,b):
+    return a/b
+
+def multiplication(a,b):
+    return a*b
+
+def soustraction(a,b):
+    return a-b
+
+def garderminchiffre(nombre): # pour avoir un nombre fini de chiffres à afficher sur la calculatrice
