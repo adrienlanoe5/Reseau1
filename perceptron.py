123144f5f07c28b2af0a1c87981591e1202ae8ef
adrienlanoe5 <adrienlanoe5@gmail.com>
2024-11-07 12:11:41 +0100

    ajout calcul valeur

--- perceptron.py
+++ perceptron.py
@@ -1,7 +1,10 @@
 class perceptron:
     def __init__(self):
+        self.w=[1,1,0.5,0.8,0.7,0.3,0.09,0.099]
+
 
     def deroulement(self):
+
 
     def fonction_activation(self):
 
@@ -10,6 +13,14 @@ class perceptron:
     def poids(self):
 
     def maj_poids(self):
+
+
+    def calcul_valeur(self,entree): # l'etrée est une liste de 0 et de 1
+        sum=0
+        for i in range(len(entrée)):
+            sum+=self.w[i]*entree[i]
+        return self.fonction_activation (sum)
+
 
 
 
