29613c45a632b255a7be4a7368bacba82a85dd11
Clement <aaazert7@gmail.com>
2024-11-07 11:48:05 +0100

    debut

---
+++ perceptron.py
@@ -0,0 +1,19 @@
+class perceptron:
+    def __init__(self):
+
+    def deroulement(self):
+
+    def fonction_activation(self):
+
+    def erreur(self):
+
+    def poids(self):
+
+    def maj_poids(self):
+
+
+
+
+
+Neurone=perceptron()
+Neurone.deroulement()
