b74e2735863468930d06d261fbd1c49c7d77bc6a
Clement <aaazert7@gmail.com>
2024-11-07 12:17:30 +0100

    debut

--- perceptron.py
+++ perceptron.py
@@ -1,11 +1,22 @@
+from math import exp
+
 class perceptron:
     def __init__(self):
-
+        self.biais=1
+        self.label= 0 # importé avec l'image
+        self.n=0.03 #taux d'apprentissage
     def deroulement(self):
 
-    def fonction_activation(self):
+    def fonction_activation(self, sum):
+        res=1 / (1 + math.exp(-sum))
+        return res
 
-    def erreur(self):
+    def erreur(self,resultat):
+        if self.label!=resultat
+            erreur=self.n(self.label-resultat)
+            return erreur
+        else :
+            return 0
 
     def poids(self):
 
