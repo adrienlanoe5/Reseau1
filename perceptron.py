2783a329e99483dcfb95c51acbb373538e73de01
adrienlanoe5 <adrienlanoe5@gmail.com>
2024-11-21 11:36:22 +0100

    archive

--- perceptron.py
+++ perceptron.py
@@ -106,13 +106,11 @@ import matplotlib.pyplot as plt
 #
 # Set file paths based on added MNIST Datasets
 
-import urllib.request
-a=urllib.request.urlretrieve("https://github.com/MrHeadbang/machineLearning/blob/main/mnist.zip", "fichier")
-input_path = r"C:\Users\adrie\PycharmProjects\TP programmation avancée\Reseau1\Reseaudeneurones\archive"
-training_images_filepath = join(input_path, 'train-images-idx3-ubyte/train-images-idx3-ubyte')
-training_labels_filepath = join(input_path, 'train-labels-idx1-ubyte/train-labels-idx1-ubyte')
-test_images_filepath = join(input_path, 't10k-images-idx3-ubyte/t10k-images-idx3-ubyte')
-test_labels_filepath = join(input_path, 't10k-labels-idx1-ubyte/t10k-labels-idx1-ubyte')
+input_path = r"/Reseaudeneurones/archive"
+training_images_filepath = input_path + 'train-images-idx3-ubyte'
+training_labels_filepath = input_path+ 'train-labels-idx1-ubyte'
+test_images_filepath = input_path+ 't10k-images-idx3-ubyte'
+test_labels_filepath = input_path+ 't10k-labels-idx1-ubyte'
 
 
 #
