39febc27cb8d586fc76b033d331e0cc523b88f31
Clement <aaazert7@gmail.com>
2024-11-21 12:22:08 +0100

    clean up

--- perceptron.py
+++ perceptron.py
@@ -119,25 +119,6 @@ test_images_filepath = 'Reseaudeneurones/archive/train-images.idx3-ubyte'
 test_labels_filepath = 'Reseaudeneurones/archive/train-labels.idx1-ubyte'
 
 
-#
-# Helper function to show a list of images with their relating titles
-#
-def show_images(images, title_texts):
-    cols = 5
-    rows = int(len(images) / cols) + 1
-    plt.figure(figsize=(30, 20))
-    index = 1
-    for x in zip(images, title_texts):
-        image = x[0]
-        title_text = x[1]
-        plt.subplot(rows, cols, index)
-        plt.imshow(image, cmap=plt.cm.gray)
-        if (title_text != ''):
-            plt.title(title_text, fontsize=15);
-        index += 1
-
-
-#
 # Load MINST dataset
 #
 mnist_dataloader = MnistDataloader(training_images_filepath, training_labels_filepath, test_images_filepath,
