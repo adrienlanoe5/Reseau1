eecf6c45367c01cee71f28db8489dfcc3e8f6a20
adrienlanoe5 <adrienlanoe5@gmail.com>
2025-03-18 22:11:25 +0100

    ajout dictionnaire resultat

--- reseau_multicouches.py
+++ reseau_multicouches.py
@@ -1,25 +1,16 @@
-import tkiteasy as tk
+import numpy as np
+from mpmath.math2 import math_sqrt
+from scipy.special import expit
+np.seterr(all='raise')
+import tkinter as tk
 
-
-g=tk.ouvrirFenetre(x=800,y=600)
-g.dessinerRectangle(x=0, y=0, l=800, h=600, col='white')
-liste=[]
-liste.append(g.attendreClic())
-print(g.recupererClic())
-while g.recupererClic()==None:
-    print(g.recupererPosition())
-    liste.append(g.recupererPosition()) #récupére l'ensemble des positions de l'utilisateur penddant le clic
-print(liste)
 
 #X_train : liste à 2 niveaux
 #1er niveau : une image
 #2ème : plusieurs listes correspondant à chacune à un niveau de l'image
 #Y_train : labels
 
-import numpy as np
-#from mpmath.math2 import math_sqrt
-#from scipy.special import expit
-#np.seterr(all='raise')
+
 class reseau_neurones():
     def __init__(self, liste_neurones, param_fonc_activation,taux_apprentissage):
         self.nb_neurones =liste_neurones
@@ -214,6 +205,23 @@ class reseau_neurones():
     def taux_reussite(self):
         return self.reussite / (self.reussite + self.defaite)
 
+    def prediction(self,image):
+        #forward propagation
+        self.archi_resultats =[]
+        self.archi_erreurs={}
+        resultat_couche=np.reshape(self.normalisation_image(image),(28*28+1,1))
+        for i in range(self.nb_couches):
+            resultat_couche=np.matmul(self.liste_poids[i],resultat_couche)
+            resultat_couche=self.fonction_activation(resultat_couche,self.param)
+            self.archi_resultats.append(resultat_couche)
+
+        vect_resultat=np.reshape(self.softmax(resultat_couche),(1,10))
+        rang_resultat=np.argmax(vect_resultat[0])
+
+        return rang_resultat
+
+
+
 
 
 # MNIST Dataset
@@ -332,6 +340,47 @@ for i in range(len(x_test)):
 print(Neurone.taux_reussite())
 
 #interface _image
+
+cote=28*20
+g=tk.ouvrirFenetre(x=cote,y=cote)
+g.dessinerRectangle(x=0, y=0, l=cote, h=cote, col='white')
+liste=[] #ensemble des positions dessinées par l'utilisateur
+while g.recupererClic()==None: #attend que l'utilisateur clique
+    pass
+while g.recupererClic()!=None: #attend la fin du clic
+    pass
+while g.recupererClic()==None: #récupère les positions avant le clic de fin
+    liste.append((g.recupererPosition().x,g.recupererPosition().y)) #récupére l'ensemble des positions de l'utilisateur penddant le clic
+grande_matrice=[[0 for i in range(cote)] for j in range(cote)] #matrice contenant l'ensemble des pixels dessinés par l'utilisateur : 1 si récupéré, 0 sinon
+for elem in liste:
+    x,y=elem
+    grande_matrice[y][x]=1
+    g.changerPixel(x, y, 'black')
+
+
+matrice=[[0 for i in range(28)] for j in range(28)]
+for x in range(28):
+    for y in range(28):
+        # récupération de l'espace associé au pixel de la matrice
+        a=(cote//28)*x
+        b=(cote//28)*(x+1)
+        c=(cote//28)*y
+        d=cote//28*(y+1)
+        lignes_matrice_pixel=grande_matrice[(cote//28)*x:(cote//28)*(x+1)]
+        matrice_pixel=[]
+        for i in range(cote//28):
+            matrice_pixel.append(lignes_matrice_pixel[i][(cote // 28) * y: (cote // 28) * (y + 1)])
+        #calcul de la coloration moyenne de la case
+        print(np.sum(matrice_pixel))
+        a=np.sum(matrice_pixel)/(cote//28)**2
+        matrice[x][y]+=a
+
+
+resultat=Neurone.prediction(matrice)
+
+
+g.attendreClic()
+
 
 
 
