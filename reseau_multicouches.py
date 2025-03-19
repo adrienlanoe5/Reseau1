a793cbb7e45ab1cdd6b41f3662fe18a8cc3e675a
Clement <aaazert7@gmail.com>
2025-03-19 21:14:08 +0100

    modif

--- reseau_multicouches.py
+++ reseau_multicouches.py
@@ -327,20 +327,19 @@ def dessin():
     cote = 28 * 20
     g = tk.ouvrirFenetre(x=cote, y=cote)
     g.dessinerRectangle(x=0, y=0, l=cote, h=cote, col='white')
-    liste = []  # ensemble des positions dessinées par l'utilisateur
+    grande_matrice = [[0 for i in range(cote)] for j in range(cote)]
+    # matrice contenant l'ensemble des pixels dessinés par l'utilisateur : 1 si récupéré, 0 sinon
     while g.recupererClic() == None:  # attend que l'utilisateur clique
         pass
     while g.recupererClic() != None:  # attend la fin du clic
         pass
     while g.recupererClic() == None:  # récupère les positions avant le clic de fin
-        liste.append((g.recupererPosition().x,
-                      g.recupererPosition().y))  # récupére l'ensemble des positions de l'utilisateur penddant le clic
-    grande_matrice = [[0 for i in range(cote)] for j in range(cote)]  # matrice contenant l'ensemble des pixels dessinés par l'utilisateur : 1 si récupéré, 0 sinon
-    for elem in liste:
-        x, y = elem
+        x, y = g.recupererPosition().x,g.recupererPosition().y
         grande_matrice[y][x] = 1
         g.changerPixel(x, y, 'black')
+        g.actualiser()
 
+    g.attendreClic()
     matrice = [[0 for i in range(28)] for j in range(28)]
     for x in range(28):
         for y in range(28):
@@ -354,17 +353,12 @@ def dessin():
             for i in range(cote // 28):
                 matrice_pixel.append(lignes_matrice_pixel[i][(cote // 28) * y: (cote // 28) * (y + 1)])
             # calcul de la coloration moyenne de la case
-            # print(np.sum(matrice_pixel))
+            print(np.sum(matrice_pixel))
             a = np.sum(matrice_pixel) / (cote // 28) ** 2
             matrice[x][y] += a
-            g.attendreClic()
+    g.attendreClic()
 
 dessin()
-
-
-
-
-
 #resultat=Neurone.prediction(matrice)
 
 
